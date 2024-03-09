47ebd15a8789c239caa9c7500ff970c926274298
Guillaume Daridon <guillaume.daridon@laposte.net>
2024-03-09 17:58:55 +0100

    test: ignore warning - coroutine 'expire_cache' was never awaited

--- test_app.py
+++ test_app.py
@@ -1,7 +1,8 @@
+import pytest
 from streamlit.testing.v1 import AppTest
 
 
+@pytest.mark.filterwarnings("ignore:coroutine")
 def test_app():
-    at = AppTest.from_file("streamlit_app.py")
-    at.run()
-    assert not at.exception
+    at = AppTest.from_file("streamlit_app.py").run()
+    at.button[0].click().run()
